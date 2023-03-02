8eebd80d19184b7997d644cd9597ab093e9a3c56
Joshua David Gardner <jgardn11@yahoo.com>
2023-03-02 16:56:37 +0000

    Complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,14 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int arr1Length = arr1.length;
+        int arr2Length = arr2.length;
+        int finalArrayLength = arr1Length + arr2Length;
+        int[] finalArray = new int[finalArrayLength];
 
-        return null;
+        System.arraycopy(arr1, 0, finalArray, 0, arr1Length);
+        System.arraycopy(arr2, 0, finalArray, arr1Length, arr2Length);
+        
+        return finalArray;
     }
 }
